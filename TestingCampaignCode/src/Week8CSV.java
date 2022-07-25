e73c758d292bb2e2860d6e1a41404dc511b86968
rmurarishetti <64725000+rmurarishetti@users.noreply.github.com>
2022-07-25 17:18:29 +0800

    Week 11 Unit and System testing

--- TestingCampaignCode/src/Week8CSV.java
+++ TestingCampaignCode/src/Week8CSV.java
@@ -1,5 +1,6 @@
 
 import java.io.*;
+import java.nio.file.Files;
 import java.util.ArrayList;
 
 import static java.lang.System.exit;
@@ -8,8 +9,8 @@ import static java.lang.System.exit;
 public class Week8CSV {
     public static void main(String[] args) {
         try{
-            FileReader file1 = new FileReader("/Users/rmurarishetti/Documents/ISTD Term 4/1005398_testingcampaign/Week 8/sample_file_1.csv");
-            FileReader file2 = new FileReader("/Users/rmurarishetti/Documents/ISTD Term 4/1005398_testingcampaign/Week 8/sample_file_2.csv");
+            File file1 = new File("/Users/rmurarishetti/Documents/ISTD Term 4/1005398_testingcampaign/Week 8/sample_file_1.csv");
+            File file2 = new File("/Users/rmurarishetti/Documents/ISTD Term 4/1005398_testingcampaign/Week 8/sample_file_2.csv");
             String path = "/Users/rmurarishetti/Documents/ISTD Term 4/1005398_testingcampaign/Week 8/";
             compareCSV(file1, file2, path);
         }
@@ -18,66 +19,93 @@ public class Week8CSV {
         }
     }
 
-    public static void compareCSV(FileReader file1, FileReader file2, String path) throws Exception {
-        BufferedReader file1Reader = new BufferedReader(file1);
-        BufferedReader file2Reader = new BufferedReader(file2);
-        Integer file1length, file2length;
-        ArrayList file1Content = new ArrayList();
-        ArrayList file2Content = new ArrayList();
-        ArrayList file3Content = new ArrayList();
+    public static boolean compareCSV(File file1, File file2, String path) throws IOException {
+        FileReader file1_reader = new FileReader(file1);
+        FileReader file2_reader = new FileReader(file2);
 
-        String file1Rows = file1Reader.readLine();
-        String file2Rows = file2Reader.readLine();
+        if (checkExtensionOfFile(file1)&&checkExtensionOfFile(file2)) {
+            BufferedReader file1Reader = new BufferedReader(file1_reader);
+            BufferedReader file2Reader = new BufferedReader(file2_reader);
+            Integer file1length, file2length;
+            ArrayList file1Content = new ArrayList();
+            ArrayList file2Content = new ArrayList();
+            ArrayList file3Content = new ArrayList();
 
-        file1length = 0;
-        while(file1Rows!=null){
-            String[] fileArray1 = file1Rows.split("\n");
-            for (String str1: fileArray1){
-                file1Content.add(str1);
-            }
-            file1Rows = file1Reader.readLine();
-            file1length++;
-        }
-        file1Reader.close();
+            String file1Rows = file1Reader.readLine();
+            String file2Rows = file2Reader.readLine();
 
-        file2length = 0;
-        while(file2Rows!=null){
-            String[] fileArray2 = file2Rows.split("\n");
-
-            for (String str2: fileArray2){
-                file2Content.add(str2);
-            }
-            file2Rows = file2Reader.readLine();
-            file2length++;
-        }
-        file2Reader.close();
-
-        if (file1length==file2length){
-            for(Object bs:file1Content)
-            {
-                if(!file2Content.contains(bs)){
-                    file3Content.add(bs);
-                    file3Content.add(file2Content.get(file1Content.indexOf(bs)));
+            file1length = 0;
+            while (file1Rows != null) {
+                String[] fileArray1 = file1Rows.split("\n");
+                for (String str1 : fileArray1) {
+                    file1Content.add(str1);
                 }
+                file1Rows = file1Reader.readLine();
+                file1length++;
             }
+            file1Reader.close();
 
-            int file3Content_size = file3Content.size();
-            try{
-                FileWriter fileWriter = new FileWriter(path+"outputComparison.csv");
+            file2length = 0;
+            while (file2Rows != null) {
+                String[] fileArray2 = file2Rows.split("\n");
 
-                for(int i=0; i<file3Content_size; i++){
-                    fileWriter.append(""+file3Content.get(i));
-                    fileWriter.append("\n");
+                for (String str2 : fileArray2) {
+                    file2Content.add(str2);
                 }
-                fileWriter.flush();
-                fileWriter.close();
-                System.out.println("Successfully Written");
-
-            }catch(IOException e){
-                e.printStackTrace();
+                file2Rows = file2Reader.readLine();
+                file2length++;
             }
+            file2Reader.close();
 
+            if (file1length.equals(file2length)) {
+                for (Object bs : file1Content) {
+                    if (!file2Content.contains(bs)) {
+                        file3Content.add(bs);
+                        file3Content.add(file2Content.get(file1Content.indexOf(bs)));
+                    }
+                }
+
+                int file3Content_size = file3Content.size();
+                try {
+                    FileWriter fileWriter = new FileWriter(path + "outputComparison.csv");
+
+                    for (int i = 0; i < file3Content_size; i++) {
+                        fileWriter.append("" + file3Content.get(i));
+                        fileWriter.append("\n");
+                    }
+                    fileWriter.flush();
+                    fileWriter.close();
+                    System.out.println("Successfully Written");
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+            } else {
+                System.out.println("File Length Not Same");
+                return false;
+            }
         }
+        else{
+            System.out.println("Wrong File Extensions");
+            return false;
+        }
+        return true;
+    }
+
+    public static Boolean checkExtensionOfFile(File file)
+    {
+        String fileExtension="";
+        // Get file Name first
+        String fileName=file.getName();
+
+        // If fileName do not contain "." or starts with "." then it is not a valid file
+        if(fileName.contains(".") && fileName.lastIndexOf(".")!= 0)
+        {
+            fileExtension=fileName.substring(fileName.lastIndexOf(".")+1);
+        }
+
+        return fileExtension.equals("csv");
 
     }
 
--- TestingCampaignCode/src/Week8CSVTest.java
+++ TestingCampaignCode/src/Week8CSVTest.java
@@ -2,28 +2,44 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 
 public class Week8CSVTest {
+
+    //Checks for FileNotFoundException
     @Test
     public void verifyFileNotFoundExeption() throws IOException {
-        FileReader file2 = new FileReader("/Users/rmurarishetti/Documents/ISTD Term 4/1005398_testingcampaign/Week 8/sample_file_2.csv");
+        File file2 = new File("/Users/rmurarishetti/Documents/ISTD Term 4/1005398_testingcampaign/Week 8/sample_file_2.csv");
         String path = "/Users/rmurarishetti/Documents/ISTD Term 4/1005398_testingcampaign/Week 8/";
-        assertThrows(FileNotFoundException.class, () -> {Week8CSV.compareCSV(new FileReader(""),file2,path);});
+        assertThrows(FileNotFoundException.class, () -> {Week8CSV.compareCSV(new File(""),file2,path);});
     }
 
+    //Checks CSV Headers for Output
     @Test
-    public void compareOutput() throws Exception {
-        FileReader file1 = new FileReader("/Users/rmurarishetti/Documents/ISTD Term 4/1005398_testingcampaign/Week 8/sample_file_1.csv");
-        FileReader file2 = new FileReader("/Users/rmurarishetti/Documents/ISTD Term 4/1005398_testingcampaign/Week 8/sample_file_3.csv");
+    public void compareHeaders() throws Exception {
+        File file1 = new File("/Users/rmurarishetti/Documents/ISTD Term 4/1005398_testingcampaign/Week 8/sample_file_1.csv");
+        File file2 = new File("/Users/rmurarishetti/Documents/ISTD Term 4/1005398_testingcampaign/Week 8/sample_file_3.csv");
         String path = "/Users/rmurarishetti/Documents/ISTD Term 4/1005398_testingcampaign/Week 8/";
         Week8CSV.compareCSV(file1, file2, path);
         BufferedReader testoutput = new BufferedReader(new FileReader("/Users/rmurarishetti/Documents/ISTD Term 4/1005398_testingcampaign/Week 8/comparsiondata.csv"));
         BufferedReader functionoutput = new BufferedReader(new FileReader("/Users/rmurarishetti/Documents/ISTD Term 4/1005398_testingcampaign/Week 8/outputcomparison.csv"));
-        assertEquals(testoutput, functionoutput);
+        assertEquals(testoutput.readLine(), functionoutput.readLine());
+    }
+
+    //Failure if wrong file extension is given
+    @Test
+    public void wrongFileType() throws Exception {
+        File file1 = new File("/Users/rmurarishetti/Documents/ISTD Term 4/1005398_testingcampaign/Week 8/W8Q1.jpeg");
+        File file2 = new File("/Users/rmurarishetti/Documents/ISTD Term 4/1005398_testingcampaign/Week 8/sample_file_3.csv");
+        String path = "/Users/rmurarishetti/Documents/ISTD Term 4/1005398_testingcampaign/Week 8/";
+        assertFalse(Week8CSV.compareCSV(file1,file2,path));
+    }
+
+    @Test
+    public void nonExistentPath(){
+        File file1 = new File("/Users/rmurarishetti/Documents/ISTD Term 4/1005398_testingcampaign/Week 8/sample_file_1.csv");
+        File file2 = new File("/Users/rmurarishetti/Documents/ISTD Term 4/1005398_testingcampaign/Week 8/sample_file_2.csv");
+        assertThrows(IOException.class,()->{Week8CSV.compareCSV(file1, file2, "/Users/userdoesntdoesntexist/Documents/ISTD Term 4/1005398_testingcampaign/Week 8/");});
     }
 
 }
