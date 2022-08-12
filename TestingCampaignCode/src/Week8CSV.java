99ced3dd31917c6945d400e18fc48d0ac6d4f043
rmurarishetti <64725000+rmurarishetti@users.noreply.github.com>
2022-08-12 15:44:29 +0800

    feat: Week 13 Fuzzing Completed

--- TestingCampaignCode/src/Week8CSV.java
+++ TestingCampaignCode/src/Week8CSV.java
@@ -63,6 +63,7 @@ public class Week8CSV {
                         file3Content.add(bs);
                         file3Content.add(file2Content.get(file1Content.indexOf(bs)));
                     }
+
                 }
 
                 int file3Content_size = file3Content.size();
@@ -105,6 +106,60 @@ public class Week8CSV {
 
         return fileExtension.equals("csv");
 
+    }
+
+    public static int getMismatches(File file1, File file2, String path) throws IOException {
+        int mismatches = 0;
+        FileReader file1_reader = new FileReader(file1);
+        FileReader file2_reader = new FileReader(file2);
+
+        if (checkExtensionOfFile(file1)&&checkExtensionOfFile(file2)) {
+            BufferedReader file1Reader = new BufferedReader(file1_reader);
+            BufferedReader file2Reader = new BufferedReader(file2_reader);
+            Integer file1length, file2length;
+            ArrayList file1Content = new ArrayList();
+            ArrayList file2Content = new ArrayList();
+            ArrayList file3Content = new ArrayList();
+
+            String file1Rows = file1Reader.readLine();
+            String file2Rows = file2Reader.readLine();
+
+            file1length = 0;
+            while (file1Rows != null) {
+                String[] fileArray1 = file1Rows.split("\n");
+                for (String str1 : fileArray1) {
+                    file1Content.add(str1);
+                }
+                file1Rows = file1Reader.readLine();
+                file1length++;
+            }
+            file1Reader.close();
+
+            file2length = 0;
+            while (file2Rows != null) {
+                String[] fileArray2 = file2Rows.split("\n");
+
+                for (String str2 : fileArray2) {
+                    file2Content.add(str2);
+                }
+                file2Rows = file2Reader.readLine();
+                file2length++;
+            }
+            file2Reader.close();
+
+
+            for (Object bs : file2Content) {
+                if (!file1Content.contains(bs)) {
+                        mismatches++;
+                }
+            }
+
+        }
+        else{
+            System.out.println("Wrong File Extensions");
+            return -1;
+        }
+        return mismatches;
     }
 
 }
